7b3b12c44fc1d1ccd7bc61285cef2594fccb6451
ericmtodd <eterictodd@gmail.com>
2021-12-15 13:29:13 -0700

    Working on adding current weather in Denver.

--- app/controllers/slide_show_controller.rb
+++ app/controllers/slide_show_controller.rb
@@ -4,21 +4,26 @@ require 'pry'
 class SlideShowController < ApplicationController
   def index
     @urls = compile_urls
+    @weather = make_weather_request
+    binding.pry
   end
   
   private
   
-  def make_request
-    
-    response = RestClient.get("https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=97fddea17599b8f6e7e5f833f30c2b9b&user_id=194608125%40N04&format=json&nojsoncallback=1&auth_token=72157720826237112-ddfe63c915adad78&api_sig=9b6a3bdc3fc4b525ff9c0a518f4fe6e9", accept: :json)
+  def make_photos_request
+    response = RestClient.get("https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=d33654f7f4ba030ab30cf81840afd79d&user_id=194608125%40N04&format=json&nojsoncallback=1&auth_token=72157720826377356-e44d986edb9cc115&api_sig=8816b2fdd530943ee97d08d7e8ef5201", accept: :json)
     json = JSON.parse(response)
     photos = json["photos"]["photo"]
+  end
+
+  def make_weather_request
+    response = RestClient.get("api.openweathermap.org/data/2.5/weather?q=Denver&appid=8d9b598296f463cb05d7baea7c741c65", accept: :json)
   end
   
   def compile_urls
     compiled_urls = Array.new
     template_url = "https://live.staticflickr.com/{server-id}/{id}_{secret}_{size-suffix}.jpg"
-    photos = make_request
+    photos = make_photos_request
     photos.each do |photo|
       new_url = "https://live.staticflickr.com/#{photo['server']}/#{photo['id']}_#{photo['secret']}_b.jpg"
       compiled_urls << new_url
